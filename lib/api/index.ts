c689aff076b66fa4121db73713e7b2ba482b43a8
Moaaz Bhnas <moaaz.bhnas@orchestrapay.com>
2025-06-20 12:36:18 +0300

    Small improvement

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -1,31 +1,25 @@
-export * from './client';
-export * from './endpoints';
-export * from './types';
-
 import { getCookie } from 'cookies-next';
 import { ApiClient } from './client';
 import { ApiEndpoints } from './endpoints';
 import { ApiConfig } from './types';
 
-const DEFAULT_BASE_URL = process.env.NEXT_PUBLIC_API_URL || '';
-
 // We don't use cookies/headers here as this is used in statically generated pages
-export function createApi({ language = 'en' }: Pick<ApiConfig, 'language'> = {}) {
+export function createApi({ language = 'en' }: Pick<ApiConfig, 'language'>) {
   const client = new ApiClient({
-    baseURL: DEFAULT_BASE_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_URL || '',
     language
   });
 
   return new ApiEndpoints(client);
 }
 
-export function createApiWithAuth({ language = 'en' }: Pick<ApiConfig, 'language'> = {}) {
+export function createApiWithAuth({ language = 'en' }: Pick<ApiConfig, 'language'>) {
   // Get tokens from cookies (works in both client and server)
   const guestToken = getCookie('guest_token');
   const authToken = getCookie('auth_token');
 
   const client = new ApiClient({
-    baseURL: DEFAULT_BASE_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_URL || '',
     language,
     guestToken,
     authToken
