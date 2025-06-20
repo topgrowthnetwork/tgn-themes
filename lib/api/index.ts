85263049d286d7d7e5c1fb131a14616416ea7949
Moaaz Bhnas <moaaz.bhnas@orchestrapay.com>
2025-06-20 12:32:33 +0300

    Small performance improvement

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -9,7 +9,17 @@ import { ApiConfig } from './types';
 
 const DEFAULT_BASE_URL = process.env.NEXT_PUBLIC_API_URL || '';
 
+// We don't use cookies/headers here as this is used in statically generated pages
 export function createApi({ language = 'en' }: Pick<ApiConfig, 'language'> = {}) {
+  const client = new ApiClient({
+    baseURL: DEFAULT_BASE_URL,
+    language
+  });
+
+  return new ApiEndpoints(client);
+}
+
+export function createApiWithAuth({ language = 'en' }: Pick<ApiConfig, 'language'> = {}) {
   // Get tokens from cookies (works in both client and server)
   const guestToken = getCookie('guest_token');
   const authToken = getCookie('auth_token');
