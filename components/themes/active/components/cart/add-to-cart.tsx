5878184464192f81652226c21c5e4e7690ddbc7e
Moaaz Bhnas <moaaz_bs@yahoo.com>
2025-06-28 09:19:23 +0300

    Small fix

--- components/themes/active/components/cart/add-to-cart.tsx
+++ components/themes/active/components/cart/add-to-cart.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { PlusIcon } from '@heroicons/react/24/outline';
-import { addItemV2 } from '@shared/cart-actions';
+import { addItemV2 } from '@shared/components/cart-actions';
 import clsx from 'clsx';
 import { ProductVariant } from 'lib/api/types';
 import { useTranslations } from 'next-intl';
