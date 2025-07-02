091a1ce539f93fc8cd6fe7022d4d856c3f5cdac6
Moaaz Bhnas <moaaz_bs@yahoo.com>
2025-07-02 16:08:38 +0300

    Small fix

--- components/themes/active/components/opengraph-image.tsx
+++ components/themes/active/components/opengraph-image.tsx
@@ -1,6 +1,5 @@
 import { createApi } from 'lib/api';
 import { getFullPath } from 'lib/utils';
-import Image from 'next/image';
 import { ImageResponse } from 'next/og';
 
 export type Props = {
@@ -19,7 +18,7 @@ export default async function OpengraphImage(props?: Props): Promise<ImageRespon
     (
       <div tw="flex h-full w-full flex-col items-center justify-center bg-black">
         <div tw="flex flex-none items-center justify-center border border-neutral-700 h-[160px] w-[160px] rounded-3xl">
-          <Image
+          <img
             src={getFullPath(globalSettings.site_logo.path)}
             alt={globalSettings.site_title}
             width={160}
