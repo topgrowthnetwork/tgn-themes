f7e481e4a48d17986132d94df2133d53ab172eba
Moaaz Bhnas <moaaz_bs@yahoo.com>
2025-07-02 22:18:33 +0300

    Small fix

--- components/themes/active/pages/category/index.tsx
+++ components/themes/active/pages/category/index.tsx
@@ -35,7 +35,7 @@ export default async function CategoryPage({
       {products.length === 0 ? (
         <p className="py-3 text-lg">{t('noProducts')}</p>
       ) : (
-        <Grid className="grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+        <Grid className="grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
           <ProductGridItems products={products} currency={settings.site_global_currency} />
         </Grid>
       )}
