bffac8ba52b5f33fe661c4af9362649573db1479
Moaaz Bhnas <moaaz.bhnas@orchestrapay.com>
2025-10-17 18:31:09 +0300

    Disabled locale detection from env vars

--- lib/i18n/routing.ts
+++ lib/i18n/routing.ts
@@ -5,5 +5,8 @@ export const routing = defineRouting({
   locales: ['en', 'ar'],
 
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: 'ar',
+
+  // Disable automatic locale detection to always default to 'ar'
+  localeDetection: process.env.DISABLE_LOCALE_DETECTION == 'true' ? false : true
 });
