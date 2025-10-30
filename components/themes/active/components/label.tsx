58b860125216c6f38fc2c7f627437ab784ff75c3
Moaaz Bhnas <moaaz.bhnas@orchestrapay.com>
2025-10-30 10:39:53 +0300

    Small fixes

--- components/themes/active/components/label.tsx
+++ components/themes/active/components/label.tsx
@@ -18,14 +18,14 @@ const Label = ({
 
   return (
     <div
-      className={clsx('absolute bottom-0 left-0 flex w-full px-4 pb-4', {
+      className={clsx('absolute bottom-0 left-0 flex w-full px-2 pb-2', {
         'lg:px-20 lg:pb-[35%]': position === 'center'
       })}
     >
-      <div className="flex w-full flex-col gap-2 rounded-2xl border bg-white/70 p-2 text-xs font-semibold text-black backdrop-blur-md">
+      <div className="flex w-full flex-col gap-1 rounded-2xl border bg-white/70 px-2 py-1.5 text-xs font-semibold text-black backdrop-blur-md">
         {/* sm:flex-row sm:items-center sm:gap-0 sm:rounded-full */}
-        <h3 className="line-clamp-1 flex-grow tracking-tight sm:me-2 sm:line-clamp-2">{title}</h3>
-        <div className="flex flex-none items-center gap-1 self-start rounded-full bg-primary-600 px-3 py-1.5 text-white sm:py-2">
+        <h3 className="line-clamp-1 flex-grow tracking-tight">{title}</h3>
+        <div className="flex flex-none items-center gap-1 self-start rounded-full bg-primary-600 px-3 py-1 text-white">
           {hasDiscount && (
             <Price
               className="text-xs line-through opacity-70"
