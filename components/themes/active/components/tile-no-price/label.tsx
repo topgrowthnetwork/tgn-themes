47d86d2abe9c0e217a7fbff2cd9b27536ee11bdc
Moaaz Bhnas <moaaz.bhnas@orchestrapay.com>
2025-11-01 09:16:06 +0200

    Small changes

--- components/themes/active/components/tile-no-price/label.tsx
+++ components/themes/active/components/tile-no-price/label.tsx
@@ -9,12 +9,12 @@ const Label = ({
 }) => {
   return (
     <div
-      className={clsx('absolute bottom-0 left-0 flex w-full px-4 pb-4', {
+      className={clsx('absolute bottom-0 left-0 flex w-full px-4 pb-2', {
         'lg:px-20 lg:pb-[35%]': position === 'center'
       })}
     >
-      <div className="flex w-full flex-col gap-2 rounded-2xl border bg-white/70 p-2 text-xs font-semibold text-black backdrop-blur-md dark:border-neutral-800 dark:bg-black/70 dark:text-white sm:flex-row sm:items-center sm:gap-0 sm:rounded-full">
-        <h3 className="line-clamp-1 flex-grow tracking-tight sm:me-2 sm:line-clamp-2">{title}</h3>
+      <div className="flex w-full flex-col gap-2 rounded-2xl border bg-white/70 px-2 py-1.5 text-sm font-semibold text-black backdrop-blur-md dark:border-neutral-800 dark:bg-black/70 dark:text-white sm:flex-row sm:items-center sm:gap-0 sm:rounded-full">
+        <h3 className="m:me-2 line-clamp-1 flex-grow sm:line-clamp-2">{title}</h3>
         {/* <div className="flex flex-none items-center gap-1 rounded-full bg-primary-600 px-2 py-1.5 text-white sm:py-2">
           {hasDiscount && (
             <Price
