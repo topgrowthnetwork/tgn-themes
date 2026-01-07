01a93ea9e0267eabc903b85f82ee7a0d7e99c54a
Moaaz Bhnas <moaaz.bhnas@orchestrapay.com>
2026-01-07 11:31:31 +0200

    Tiktok pixel

--- app/[language]/layout.tsx
+++ app/[language]/layout.tsx
@@ -82,6 +82,9 @@ export default async function LocaleLayout({
   // Get Meta Pixel ID from environment variable
   const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
 
+  // Get TikTok Pixel ID from environment variable
+  const tiktokPixelId = process.env.NEXT_PUBLIC_TIKTOK_PIXEL_ID;
+
   return (
     <html
       lang={language}
@@ -122,6 +125,51 @@ export default async function LocaleLayout({
               />
             </noscript>
           </>
+        )}
+        {/* TikTok Pixel Code */}
+        {tiktokPixelId && (
+          <Script
+            id="tiktok-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function (w, d, t) {
+                  w.TiktokAnalyticsObject=t;
+                  var ttq=w[t]=w[t]||[];
+                  ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+                  ttq.setAndDefer=function(t,e){
+                    t[e]=function(){
+                      t.push([e].concat(Array.prototype.slice.call(arguments,0)))
+                    }
+                  };
+                  for(var i=0;i<ttq.methods.length;i++) ttq.setAndDefer(ttq,ttq.methods[i]);
+                  ttq.instance=function(t){
+                    for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++) ttq.setAndDefer(e,ttq.methods[n]);
+                    return e
+                  };
+                  ttq.load=function(e,n){
+                    var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;
+                    ttq._i=ttq._i||{};
+                    ttq._i[e]=[];
+                    ttq._i[e]._u=r;
+                    ttq._t=ttq._t||{};
+                    ttq._t[e]=+new Date;
+                    ttq._o=ttq._o||{};
+                    ttq._o[e]=n||{};
+                    n=d.createElement("script");
+                    n.type="text/javascript";
+                    n.async=!0;
+                    n.src=r+"?sdkid="+e+"&lib="+t;
+                    e=d.getElementsByTagName("script")[0];
+                    e.parentNode.insertBefore(n,e)
+                  };
+
+                  ttq.load('${tiktokPixelId}');
+                  ttq.page();
+                }(window, document, 'ttq');
+              `
+            }}
+          />
         )}
         <TamaraWidgetScript language={language} country="SA" />
         <TabbyWidgetScript language={language} />
