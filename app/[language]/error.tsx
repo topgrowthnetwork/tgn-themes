26fe5ed0e8b700dacdcf8873798422b93a9324da
Moaaz Bhnas <moaaz.bhnas@orchestrapay.com>
2026-02-09 14:54:43 +0200

    Added back to home button in the error view

--- app/[language]/error.tsx
+++ app/[language]/error.tsx
@@ -1,16 +1,23 @@
 'use client';
 
+import { useTranslations } from 'next-intl';
+import Link from 'next/link';
+
 export default function Error({ reset }: { reset: () => void }) {
+  const t = useTranslations('Error');
+
   return (
-    <div className="mx-auto my-4 flex max-w-xl flex-col rounded-theme border border-neutral-200 bg-white p-8 md:p-12 dark:border-neutral-800 dark:bg-black">
-      <h2 className="text-xl font-bold">Oh no!</h2>
-      <p className="my-2">
-        There was an issue with our storefront. This could be a temporary issue, please try your
-        action again.
-      </p>
-      <button className="button mx-auto mt-4 w-full" onClick={() => reset()}>
-        Try Again
-      </button>
+    <div className="mx-auto my-4 flex max-w-xl flex-col rounded-theme border border-neutral-200 bg-white p-8 dark:border-neutral-800 dark:bg-black md:p-12">
+      <h2 className="text-xl font-bold">{t('title')}</h2>
+      <p className="my-2">{t('description')}</p>
+      <div className="mx-auto mt-4 flex w-full flex-col gap-2">
+        <button className="button w-full" onClick={() => reset()}>
+          {t('tryAgain')}
+        </button>
+        <Link href={'/'} className="button button-secondary w-full text-center">
+          {t('goToHomepage')}
+        </Link>
+      </div>
     </div>
   );
 }
